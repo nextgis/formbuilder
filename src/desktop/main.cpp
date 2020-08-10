7ed6c48e5b28b45e6ae0b81b4cad71b9fdda99f1
Mikhail Gusev <gusevmihs@gmail.com>
2020-08-10 16:06:11 +0300

    Remove PROJ_LIB for MacOS

--- src/desktop/main.cpp
+++ src/desktop/main.cpp
@@ -97,9 +97,11 @@ void installTranslators (const QApplication &app, const QStringList &tr_names)
 
 int main (int argc, char *argv[])
 {
+    #ifdef Q_OS_WIN
     #if GDAL_VERSION_MAJOR >= 3
     QByteArray ba_proj_db = QString("../share/proj").toUtf8();
     qputenv("PROJ_LIB", ba_proj_db.data());
+    #endif
     #endif
     
     QApplication app(argc, argv);
